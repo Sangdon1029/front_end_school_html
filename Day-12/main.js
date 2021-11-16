dcf939c9003a1e542d0e8cc7f6be9bacbe85d294
Sangdon1029 <93380732+Sangdon1029@users.noreply.github.com>
2021-11-16 21:50:46 +0900

    Grid 정리

---
+++ Day-12/main.js
@@ -0,0 +1,14 @@
+const 제거 = document.getElementsByClassName('test');
+const 추가 = document.getElementsByClassName('test');
+
+function toggle() {
+	element.classList.toggle('test-class');
+}
+
+function add() {
+	element.classList.add('test-class');
+}
+
+function remove() {
+	element.classList.remove('test-class');
+}
